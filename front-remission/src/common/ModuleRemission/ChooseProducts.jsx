9068e0bd4c25aa3e3464bd30a09eeed4a9d78943
melendez-dev <sams20030818@gmail.com>
2023-05-09 15:33:08 -0500

    feature: finish remission

--- front-remission/src/common/ModuleRemission/ChooseProducts.jsx
+++ front-remission/src/common/ModuleRemission/ChooseProducts.jsx
@@ -24,411 +24,65 @@ const ChooseProducts = ({
   productsSelected,
   isEdit = false,
 }) => {
+
+  const [productList, setProductList] = useState(productsSelected?.length ? productsSelected?.length : 1)
+
+  const handleAddProduct = () => {
+    if (productList > 9){
+      return
+    }else{
+      setProductList(prev => prev + 1)
+    }
+  }
+
+  const handleSubProduct = () => {
+    if (productList == 1){
+      return
+    }else{
+      setProductList(prev => prev - 1)
+      setProductSelected((previousArr) => (previousArr.slice(0, -1)))
+    }
+  }
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Box display="flex" alignItems="center" justifyContent="space-between">
-          <Box style={{ cursor: "pointer" }}>
+          <Box style={{ cursor: "pointer" }} onClick={handleSubProduct}>
             <RiSubtractFill />
           </Box>
           <Box>Cantidad</Box>
-          <Box style={{ cursor: "pointer" }}>
+          <Box style={{ cursor: "pointer" }} onClick={handleAddProduct}>
             <AiOutlinePlus />
           </Box>
         </Box>
       </Grid>
       {/* products */}
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
+      {Array.from({length: productList})?.map((el, i) => (
+      <Grid item xs={12} key={i}>
+        <Autocomplete
+          id="products"
+          options={products}
+          defaultValue={productsSelected[i] ??  null}
+          getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
+          onChange={(_event, newValue) => {
+            if (newValue) {
+              setProductSelected((prev) => [...prev, newValue]);
+            } else {
+              productsSelected[i] = null;
+            }
           }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              defaultValue={productsSelected[0] ?? null}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[0] = null;
-                }
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 1"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
+          renderInput={(params) => (
+             <TextField
+              {...params}
+              fullWidth
+              size="small"
+              variant="outlined"
+              label="Producto"
+             />
+           )}
+         />
       </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              defaultValue={productsSelected[1] ?? null}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[1] = null;
-                }
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 2"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[2] = null;
-                }
-              }}
-              defaultValue={productsSelected[2] ?? null}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 3"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[3] = null;
-                }
-              }}
-              defaultValue={productsSelected[3] ?? null}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 4"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[4] = null;
-                }
-              }}
-              defaultValue={productsSelected[4] ?? null}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 5"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              defaultValue={productsSelected[5] ?? null}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[5] = null;
-                }
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 6"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              defaultValue={productsSelected[6] ?? null}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[6] = null;
-                }
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 7"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[7] = null;
-                }
-              }}
-              defaultValue={productsSelected[7] ?? null}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 8"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[8] = null;
-                }
-              }}
-              defaultValue={productsSelected[8] ?? null}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 9"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
-      <Grid item xs={12}>
-        <Controller
-          rules={{
-            required: {
-              value: false,
-              message: "Este campo es obligatorio",
-            },
-          }}
-          name="products"
-          control={control}
-          render={({ field }) => (
-            <Autocomplete
-              id="products"
-              options={products}
-              getOptionLabel={(option) => `${option?.name} - ${option?.code}`}
-              onChange={(_event, newValue) => {
-                if (newValue) {
-                  setProductSelected((prev) => [...prev, newValue]);
-                } else {
-                  productsSelected[9] = null;
-                }
-              }}
-              defaultValue={productsSelected[9] ?? null}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  fullWidth
-                  size="small"
-                  variant="outlined"
-                  label="Producto 10"
-                />
-              )}
-            />
-          )}
-        />
-        {errors?.products && (
-          <FormHelperText error>{errors?.products?.message}</FormHelperText>
-        )}
-      </Grid>
+      ))}
 
       <Grid item xs={6}>
         <Button
